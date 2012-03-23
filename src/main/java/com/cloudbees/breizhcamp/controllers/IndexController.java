3cf6ca3942a41bfb7f8298acd09674a83082e74c
Alexandre THOMAZO <alex@thomazo.info>
2012-03-23 21:36:59 +0100

    Using Spring MVC as web framework

---
+++ src/main/java/com/cloudbees/breizhcamp/controllers/IndexController.java
@@ -0,0 +1,29 @@
+package com.cloudbees.breizhcamp.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.cloudbees.breizhcamp.Schedule;
+
+/**
+ * Handle request for index page
+ * 
+ * @author Alexandre THOMAZO <alex@thomazo.info>
+ */
+@Controller
+public class IndexController {
+	
+	@Autowired
+	private Schedule schedule;
+	
+	@RequestMapping("/")
+	public String index(ModelMap model, @RequestParam(defaultValue="Amphi") String room) {
+		
+		model.put("talks", schedule.getSchedule(room));
+		return "index";
+	}
+
+}
