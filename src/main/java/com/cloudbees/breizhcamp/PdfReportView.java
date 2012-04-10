3da9641ce7572bc91d94ed8924029ea41c16549d
ybonnel <ybonnel@gmail.com>
2012-04-10 23:37:32 +0200

    Début de mise en forme du pdf (titre et header du tableau).

--- src/main/java/com/cloudbees/breizhcamp/PdfReportView.java
+++ src/main/java/com/cloudbees/breizhcamp/PdfReportView.java
@@ -5,17 +5,34 @@ import com.cloudbees.breizhcamp.domain.Room;
 import com.cloudbees.breizhcamp.domain.Speaker;
 import com.cloudbees.breizhcamp.domain.Talk;
 import com.lowagie.text.*;
+import com.lowagie.text.pdf.PdfPCell;
+import com.lowagie.text.pdf.PdfPTable;
 import com.lowagie.text.pdf.PdfWriter;
 import org.springframework.web.servlet.view.document.AbstractPdfView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.awt.*;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Map;
 
 
 public class PdfReportView extends AbstractPdfView {
+    
+    private PdfPCell createHeaderCell(String content) throws BadElementException {
+        Paragraph paragraph = new Paragraph();
+        Font font = new Font();
+        font.setColor(Color.WHITE);
+        paragraph.setFont(font);
+        paragraph.add(new Phrase(content));
+        PdfPCell cell = new PdfPCell(paragraph);
+        cell.setPaddingBottom(10);
+        cell.setHorizontalAlignment(Cell.ALIGN_CENTER);
+        cell.setBackgroundColor(Color.GRAY);
+        return cell;
+    }
+    
     @Override
     protected void buildPdfDocument(Map<String, Object> model, Document document, PdfWriter pdfWriter,
                                     HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse)
@@ -29,42 +46,48 @@ public class PdfReportView extends AbstractPdfView {
         document.setPageSize(PageSize.A4.rotate());
 
         for (Date date : data.getDatesOrdonnees()) {
-            document.add(new Paragraph("Programme du " + sdf.format(date)));
+            Paragraph titre = new Paragraph();
+            Font font = new Font();
+            font.setStyle(Font.BOLD);
+            font.setSize(14);
+            titre.setFont(font);
+            titre.setAlignment(Paragraph.ALIGN_CENTER);
+            titre.add(new Phrase("Programme du " + sdf.format(date)));
+            document.add(titre);
 
-            Table table = new Table(data.getRooms().size() + 1);
-            table.addCell("Heure");
+            PdfPTable table = new PdfPTable(data.getRooms().size() + 1);
+            table.setWidthPercentage(100);
+            table.setSpacingBefore(10);
+            table.setSpacingAfter(20);
+
+            table.addCell(createHeaderCell("Heure"));
             for (Room room : data.getRooms()) {
-                table.addCell(room.getName());
+                table.addCell(createHeaderCell(room.getName()));
             }
             for (String creneau : data.getCreneaux().get(date)) {
                 table.addCell(creneau);
                 if (data.getTalks().get(date).get(creneau).containsKey(sansRoom)) {
-                    Cell cell = new Cell();
+                    PdfPCell cell = new PdfPCell();
                     cell.setColspan(data.getRooms().size());
                     Talk talk = data.getTalks().get(date).get(creneau).get(sansRoom);
-                    cell.add(new Paragraph(talk.getTitle()));
-                    cell.add(new Paragraph(talk.getTheme().getHtmlValue()));
+                    cell.addElement(new Paragraph(talk.getTitle()));
+                    cell.addElement(new Paragraph(talk.getTheme().getHtmlValue()));
                     for (Speaker speaker : talk.getSpeakers()) {
-                        if (speaker.getPicture() != null) {
-                            Image image = Image.getInstance(speaker.getPicture());
-                            image.scaleAbsolute(30, 30);
-                            cell.add(image);
-                        }
+                        cell.addElement(new Paragraph(speaker.getFirstName() + " " + speaker.getLastName()));
                     }
                     table.addCell(cell);
                 } else {
                     for (Room room : data.getRooms()) {
-                        Cell cell = new Cell();
+                        PdfPCell cell = new PdfPCell();
 
                         Talk talk = data.getTalks().get(date).get(creneau).get(room.getName());
                         if (talk != null) {
-                            cell.add(new Paragraph(talk.getTitle()));
-                            cell.add(new Paragraph(talk.getTheme().getHtmlValue()));
+                            cell.addElement(new Paragraph(talk.getTitle()));
+                            cell.addElement(new Paragraph(talk.getTheme().getHtmlValue()));
                             for (Speaker speaker : talk.getSpeakers()) {
                                 if (speaker.getPicture() != null) {
-                                    Image image = Image.getInstance(speaker.getPicture());
-                                    image.scaleAbsolute(30, 30);
-                                    cell.add(image);
+                                    cell.addElement(new Paragraph(speaker.getFirstName() + " " + speaker.getLastName
+                                            ()));
                                 }
                             }
                         }
--- src/main/java/com/cloudbees/breizhcamp/controllers/ReportController.java
+++ src/main/java/com/cloudbees/breizhcamp/controllers/ReportController.java
@@ -4,9 +4,14 @@ package com.cloudbees.breizhcamp.controllers;
 import com.cloudbees.breizhcamp.services.ScheduleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 public class ReportController {
@@ -18,5 +23,19 @@ public class ReportController {
     protected ModelAndView programme() throws Exception {
         return new ModelAndView("pdfViewResolver", "data", service.getData());
 
+    }
+    
+    /**
+     * Launched if an error appears
+     * @param e Exception raised
+     * @return Error message
+     */
+    @ExceptionHandler
+    @ResponseBody
+    public Map<String, String> handleException(Exception e) {
+        e.printStackTrace();
+        Map<String, String> res = new HashMap<String, String>();
+        res.put("error", e.getLocalizedMessage());
+        return res;
     }
 }
