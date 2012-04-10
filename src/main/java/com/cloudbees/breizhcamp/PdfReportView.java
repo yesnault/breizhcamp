6d0b7f0c7c800045b510848d8722b1c61fda7560
Sylvain Guernion <gsylvain35@gmail.com>
2012-04-11 00:17:00 +0200

    mise en page pdf

--- src/main/java/com/cloudbees/breizhcamp/PdfReportView.java
+++ src/main/java/com/cloudbees/breizhcamp/PdfReportView.java
@@ -4,13 +4,10 @@ import com.cloudbees.breizhcamp.controllers.Data;
 import com.cloudbees.breizhcamp.domain.Room;
 import com.cloudbees.breizhcamp.domain.Speaker;
 import com.cloudbees.breizhcamp.domain.Talk;
-import com.lowagie.text.BadElementException;
-import com.lowagie.text.Cell;
-import com.lowagie.text.Document;
+import com.lowagie.text.*;
 import com.lowagie.text.Font;
-import com.lowagie.text.PageSize;
-import com.lowagie.text.Paragraph;
-import com.lowagie.text.Phrase;
+import com.lowagie.text.Image;
+import com.lowagie.text.Rectangle;
 import com.lowagie.text.pdf.PdfPCell;
 import com.lowagie.text.pdf.PdfPTable;
 import com.lowagie.text.pdf.PdfWriter;
@@ -19,13 +16,22 @@ import org.springframework.web.servlet.view.document.AbstractPdfView;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.awt.*;
+import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Map;
 
 
 public class PdfReportView extends AbstractPdfView {
-    
+
+
+    Font talkFont =
+            FontFactory.getFont(FontFactory.HELVETICA, Font.DEFAULTSIZE,
+                    Font.NORMAL, Color.BLACK);
+    Font themeFont =
+            FontFactory.getFont(FontFactory.HELVETICA, 8,
+                    Font.ITALIC, Color.GRAY);
+
     private PdfPCell createHeaderCell(String content) throws BadElementException {
         Paragraph paragraph = new Paragraph();
         Font font = new Font();
@@ -44,6 +50,23 @@ public class PdfReportView extends AbstractPdfView {
         return new Document(PageSize.A4.rotate());
     }
 
+    private void createFirstPage(Document document) throws DocumentException, IOException {
+        Rectangle savePagesize = document.getPageSize();
+        document.setPageSize(PageSize.A4);
+        document.newPage();
+        Image imageLogo = Image.getInstance("http://localhost:8080/static/BreizhCamp.png");
+        imageLogo.scaleAbsolute(document.getPageSize().getWidth() - document.leftMargin() - document.rightMargin(), imageLogo.getHeight());
+        document.add(imageLogo);
+
+        Paragraph paragraph = new Paragraph("14 et 15 Juin 2012");
+        paragraph.setSpacingAfter(25);
+        paragraph.getFont().setSize(25);
+        paragraph.setAlignment(Element.ALIGN_CENTER);
+        document.add(paragraph);
+
+        document.setPageSize(savePagesize);
+    }
+
     @Override
     protected void buildPdfDocument(Map<String, Object> model, Document document, PdfWriter pdfWriter,
                                     HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse)
@@ -53,6 +76,12 @@ public class PdfReportView extends AbstractPdfView {
         SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 
         String sansRoom = "sansRoom";
+
+        HeaderFooter footer = new HeaderFooter(new Phrase("BreizhCamp 2012"), false);
+        document.setFooter(footer);
+
+        createFirstPage(document);
+        document.newPage();
 
         for (Date date : data.getDatesOrdonnees()) {
             Paragraph titre = new Paragraph();
@@ -79,8 +108,8 @@ public class PdfReportView extends AbstractPdfView {
                     PdfPCell cell = new PdfPCell();
                     cell.setColspan(data.getRooms().size());
                     Talk talk = data.getTalks().get(date).get(creneau).get(sansRoom);
-                    cell.addElement(new Paragraph(talk.getTitle()));
-                    cell.addElement(new Paragraph(talk.getTheme().getHtmlValue()));
+                    cell.addElement(new Paragraph(new Phrase(talk.getTitle(), talkFont)));
+                    cell.addElement(new Paragraph(new Phrase(talk.getTheme().getHtmlValue(), themeFont)));
                     for (Speaker speaker : talk.getSpeakers()) {
                         cell.addElement(new Paragraph(speaker.getFirstName() + " " + speaker.getLastName()));
                     }
@@ -91,8 +120,8 @@ public class PdfReportView extends AbstractPdfView {
 
                         Talk talk = data.getTalks().get(date).get(creneau).get(room.getName());
                         if (talk != null) {
-                            cell.addElement(new Paragraph(talk.getTitle()));
-                            cell.addElement(new Paragraph(talk.getTheme().getHtmlValue()));
+                            cell.addElement(new Paragraph(new Phrase(talk.getTitle(), talkFont)));
+                            cell.addElement(new Paragraph(new Phrase(talk.getTheme().getHtmlValue(), themeFont)));
                             for (Speaker speaker : talk.getSpeakers()) {
                                 if (speaker.getPicture() != null) {
                                     cell.addElement(new Paragraph(speaker.getFirstName() + " " + speaker.getLastName
